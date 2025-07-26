1a8a3c68fb478cb0ddfa08c7727de892079b50b4
Arvind Murari Vepa <arvind.m.vepa@gmail.com>
2025-07-27 00:47:13 +0100

    first attempt adapting to brats

--- src/dataset/mllm_dataset.py
+++ src/dataset/mllm_dataset.py
@@ -4,7 +4,6 @@ import torch
 import random
 import numpy as np
 import monai.transforms as mtf
-import SimpleITK as sitk
 import pandas as pd
 from monai.data import set_track_meta
 from torch.utils.data import Dataset, ConcatDataset
@@ -64,11 +63,8 @@ class CapDataset(Dataset):
                 image_path = data["image"]
                 image_abs_path = os.path.join(self.data_root, image_path)
 
-                # image = np.load(image_abs_path)  # nomalized 0-1, C,D,H,W
+                image = np.load(image_abs_path)  # nomalized 0-1, C,D,H,W
                 # image = np.load(img_abs_path)[np.newaxis, ...]  # nomalized
-                image = sitk.ReadImage(image_abs_path)
-                image = sitk.GetArrayFromImage(image)
-                image = np.expand_dims(image, axis=0)
                 image = self.transform(image)
 
                 text_path = data["text"]
@@ -145,11 +141,11 @@ class VQADataset(Dataset):
         self.image_tokens = "<im_patch>" * args.proj_out_num
 
         if mode == "train":
-            self.data_list = pd.read_csv(args.vqa_data_train_path)
+            self.data_list = self.read_data_file(args.vqa_data_train_path)
         elif mode == "validation":
-            self.data_list = pd.read_csv(args.vqa_data_val_path, nrows=2048)
+            self.data_list = self.read_data_file(args.vqa_data_val_path, nrows=2048)
         elif "test" in mode:
-            self.data_list = pd.read_csv(args.vqa_data_test_path)
+            self.data_list = self.read_data_file(args.vqa_data_test_path)
         else:
             print("The mode is not desired ! ")
 
@@ -189,11 +185,8 @@ class VQADataset(Dataset):
                 data = self.data_list.iloc[idx]
                 image_abs_path = os.path.join(self.args.data_root, data["Image Path"])
 
-                # image = np.load(image_abs_path)  # nomalized, 0-1, C,D,H,W
+                image = np.load(image_abs_path)  # nomalized, 0-1, C,D,H,W
                 # image = np.load(img_path)[np.newaxis, ...]  # nomalized
-                image = sitk.ReadImage(image_abs_path)
-                image = sitk.GetArrayFromImage(image)
-                image = np.expand_dims(image, axis=0)
                 image = self.transform(image)
 
                 if self.close_ended:
@@ -263,7 +256,91 @@ class VQADataset(Dataset):
             except Exception as e:
                 print(f"Error in __getitem__ at index {idx}: {e}")
                 idx = random.randint(0, len(self.data_list) - 1)
+    
+    def read_data_file(self, vqa_data_file, **kwargs):
+        data_list = pd.read_csv(vqa_data_file, **kwargs)
+        return data_list
+    
 
+class VQABratsDataset(VQADataset):
+
+    def read_data_file(self, vqa_data_file, **kwargs):
+        with open(vqa_data_file, 'r') as f:
+            data_list = json.load(f)
+        return data_list
+
+    def __getitem__(self, idx):
+        # TODO: Figure out how to use more modalities
+        data = self.data_list[idx]
+
+        image = []
+        for modality in ["t1c", "t1n", "t2f", "t2w"]:
+            image_abs_path = data["volume_non_seg_files"][modality]
+            new_image_abs_path = self.convert_file_path_to_npy(image_abs_path)
+            image_ = np.load(new_image_abs_path)
+            image_ = self.transform(image_)
+            image.append(image_)
+        image = torch.stack(image, axis=0)
+
+        question = data["question"]
+        answer = str(data["answer"])
+
+
+        question = self.image_tokens + ' ' + question
+        text_tensor = self.tokenizer(
+            question + ' ' + answer, max_length=self.args.max_length, truncation=True, padding="max_length", return_tensors="pt",
+        )
+
+        input_id = text_tensor["input_ids"][0]
+        attention_mask = text_tensor["attention_mask"][0]
+
+        valid_len = torch.sum(attention_mask)
+        if valid_len < len(input_id):
+            input_id[valid_len] = self.tokenizer.eos_token_id
+
+        question_tensor = self.tokenizer(
+            question, max_length=self.args.max_length, truncation=True, padding="max_length", return_tensors="pt"
+        )
+        question_len = torch.sum(question_tensor["attention_mask"][0])
+
+        label = input_id.clone()
+        label[:question_len] = -100
+        if self.tokenizer.pad_token_id == self.tokenizer.eos_token_id:
+            label[label == self.tokenizer.pad_token_id] = -100
+            if valid_len < len(label):
+                label[valid_len] = self.tokenizer.eos_token_id
+        else:
+            label[label == self.tokenizer.pad_token_id] = -100
+
+        ret = {
+            'image': image,
+            'input_id': input_id,
+            'label': label,
+            'attention_mask': attention_mask,
+            'question': question,
+            'answer': answer,
+            'answer_choice': answer,
+            'question_type': 'open_ended',
+            'qid': data["qid"],
+            'volume_file_id': data["volume_file_id"],
+            'volume_file_dir': data["volume_file_dir"],
+            'question_clean': data["question"],
+            'label_name': data["label_name"],
+            'q_lang': data["q_lang"],
+            'content_type': data["content_type"],
+            'study_name': data["study_name"],
+        }
+        return ret
+
+    def convert_file_path_to_npy(self, image_abs_path):
+        volume_abs_dir = os.path.dirname(image_abs_path)
+        base_dir = os.path.dirname(volume_abs_dir)
+        new_base_dir = base_dir + "_npy"
+
+        volume_dir = os.path.basename(volume_abs_dir)
+        image_file = os.path.basename(image_abs_path)
+        new_image_abs_path = os.path.join(new_base_dir, volume_dir, image_file + ".npy")
+        return new_image_abs_path
 
 class VQAYNDataset(Dataset):
     def __init__(self, args, tokenizer, mode="train"):
@@ -319,11 +396,8 @@ class VQAYNDataset(Dataset):
                 data = self.data_list.iloc[idx]
                 image_abs_path = os.path.join(self.args.data_root, data["Image Path"])
 
-                # image = np.load(image_abs_path)  # nomalized, 0-1, C,D,H,W
+                image = np.load(image_abs_path)  # nomalized, 0-1, C,D,H,W
                 # image = np.load(img_path)[np.newaxis, ...]  # nomalized
-                image = sitk.ReadImage(image_abs_path)
-                image = sitk.GetArrayFromImage(image)
-                image = np.expand_dims(image, axis=0)
                 image = self.transform(image)
 
                 question = data["Question"]
@@ -387,10 +461,8 @@ class TextDatasets(Dataset):
     def __init__(self, args, tokenizer, mode="train"):
         super(TextDatasets, self).__init__()
         self.ds_list = [
-            CapDataset(args, tokenizer, mode),
-            VQADataset(args, tokenizer, close_ended=True, mode=mode),
-            VQADataset(args, tokenizer, close_ended=False, mode=mode),
-        ]
+            VQABratsDataset(args, tokenizer, mode)
+            ]
         self.dataset = ConcatDataset(self.ds_list)
 
     def __len__(self):
@@ -404,11 +476,8 @@ class TextYNDatasets(Dataset):
     def __init__(self, args, tokenizer, mode="train"):
         super(TextYNDatasets, self).__init__()
         self.ds_list = [
-            CapDataset(args, tokenizer, mode),
-            VQADataset(args, tokenizer, close_ended=True, mode=mode),
-            VQADataset(args, tokenizer, close_ended=False, mode=mode),
-            VQAYNDataset(args, tokenizer, mode),
-        ]
+            VQABratsDataset(args, tokenizer, mode)
+            ]
         self.dataset = ConcatDataset(self.ds_list)
 
     def __len__(self):
