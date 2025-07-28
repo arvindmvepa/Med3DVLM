9d26fb9398099b66393a7feff56f2cf99bad7e73
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-07-28 18:49:54 +0100

    eval stuff

---
+++ scripts/eval/eval_brats1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+output_dir=./output/finetuned-model-new-dataset-0000
+train_path=/local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_train_updated_v2_seed0_multitask_fixed.json
+val_path=/local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_val_updated_v2_seed0_multitask_fixed.json
+test_path=/local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_test_updated_v2_seed0_multitask_fixed.json
+
+PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python src/eval/eval_vqa.py \
+    --model_name_or_path "$output_dir"/hf \
+    --vqa_data_test_path $test_path \
+    --max_length 512 \
+    --proj_out_num 256 \
+    --do_sample \
+    --output_dir $output_dir/eval_vqa \
+
+PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python src/eval/eval_vqa_utils.py \
+--output_dir $output_dir/eval_vqa \
+--gt_file $test_path \
