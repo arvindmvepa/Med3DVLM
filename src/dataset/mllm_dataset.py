a5a429d7b46e1500a7624af285bdd0501f5d0515
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-07-31 04:17:04 +0100

    bug fix

--- src/dataset/mllm_dataset.py
+++ src/dataset/mllm_dataset.py
@@ -322,13 +322,13 @@ class VQABratsDataset(VQADataset):
             'answer_choice': answer,
             'question_type': 'open_ended',
             'qid': data.get("qid", 0),
-            'volume_file_id': data["volume_file_id"],
-            'volume_file_dir': data["volume_file_dir"],
-            'question_clean': data["question"],
-            'label_name': data["label_name"],
-            'q_lang': data["q_lang"],
-            'content_type': data["content_type"],
-            'study_name': data["study_name"],
+            'volume_file_id': data.get("volume_file_id"),
+            'volume_file_dir': data.get("volume_file_dir"),
+            'question_clean': data.get("question"),
+            'label_name': data.get("label_name"),
+            'q_lang': data.get("q_lang"),
+            'content_type': data.get("content_type"),
+            'study_name': data.get("study_name"),
         }
         return ret
 
