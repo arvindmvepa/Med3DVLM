ce15fb28eb599cce9ca58d04a79ae90bf3bde6a8
Arvind Murari Vepa <arvind.m.vepa@gmail.com>
2025-11-30 00:39:39 -0800

    inference for new dataset

--- scripts/eval/eval_brats.sh
+++ scripts/eval/eval_brats.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-output_dir=./output/finetuned-model-new-dataset-v1-0000
-train_path=/local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_train_updated_v10_seed0_multitask_fixed.json
-val_path=/local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_val_updated_v10_seed0_multitask_fixed.json
-test_path=/local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_test_updated_v10_seed0_multitask_fixed.json
+output_dir=./output/finetuned-model-new-dataset-v11-0000
+train_path=/local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_train_updated_v11_seed0_multitask_fixed.json
+val_path=/local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_val_updated_v11_seed0_multitask_fixed.json
+test_path=/local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_test_updated_v11_seed0_multitask_fixed.json
 
 PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python src/eval/eval_vqa.py \
     --model_name_or_path "$output_dir"/hf \
