374837e2841b4d354094fb8e09b303142e9351f8
Arvind Murari Vepa <arvind.m.vepa@gmail.com>
2026-01-23 17:42:10 -0800

    do inference for m3d3dvlm

--- scripts/eval/eval_brats.sh
+++ scripts/eval/eval_brats.sh
@@ -3,7 +3,7 @@
 output_dir=./output/finetuned-model-new-dataset-v11-0000
 train_path=/local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_train_updated_v11_seed0_multitask_fixed.json
 val_path=/local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_val_updated_v11_seed0_multitask_fixed.json
-test_path=/local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_test_updated_v11_seed0_multitask_fixed.json
+test_path=/local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_test_updated_v11_seed0_filt_multitask_fixed.json
 
 PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python src/eval/eval_vqa.py \
     --model_name_or_path "$output_dir"/hf \
@@ -11,17 +11,17 @@ PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python src/eval/eval_vqa.py \
     --max_length 512 \
     --proj_out_num 256 \
     --do_sample \
-    --output_dir $output_dir/eval_vqa \
+    --output_dir $output_dir/eval_vqa1 \
 
 PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python src/eval/eval_vqa_utils.py \
---output_dir $output_dir/eval_vqa \
+--output_dir $output_dir/eval_vqa1 \
 --gt_file $test_path \
 
 
 output_dir=./output/finetuned-model-new-dataset-met-v11-0000
 train_path=/local2/amvepa91/MedTrinity-25M/brats_met_3d_vqa_subjTrue_train_updated_v11_seed0_multitask_fixed.json
 val_path=/local2/amvepa91/MedTrinity-25M/brats_met_3d_vqa_subjTrue_val_updated_v11_seed0_multitask_fixed.json
-test_path=/local2/amvepa91/MedTrinity-25M/brats_met_3d_vqa_subjTrue_test_updated_v11_seed0_multitask_fixed.json
+test_path=/local2/amvepa91/MedTrinity-25M/brats_met_3d_vqa_subjTrue_test_updated_v11_seed0_filt_multitask_fixed.json
 
 PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python src/eval/eval_vqa.py \
     --model_name_or_path "$output_dir"/hf \
@@ -29,17 +29,17 @@ PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python src/eval/eval_vqa.py \
     --max_length 512 \
     --proj_out_num 256 \
     --do_sample \
-    --output_dir $output_dir/eval_vqa \
+    --output_dir $output_dir/eval_vqa1 \
 
 PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python src/eval/eval_vqa_utils.py \
---output_dir $output_dir/eval_vqa \
+--output_dir $output_dir/eval_vqa1 \
 --gt_file $test_path \
 
 
 output_dir=./output/finetuned-model-new-dataset-goat-v11-0000
 train_path=/local2/amvepa91/MedTrinity-25M/brats_goat_3d_vqa_subjTrue_train_updated_v11_seed0_multitask_fixed.json
 val_path=/local2/amvepa91/MedTrinity-25M/brats_goat_3d_vqa_subjTrue_val_updated_v11_seed0_multitask_fixed.json
-test_path=/local2/amvepa91/MedTrinity-25M/brats_goat_3d_vqa_subjTrue_test_updated_v11_seed0_multitask_fixed.json
+test_path=/local2/amvepa91/MedTrinity-25M/brats_goat_3d_vqa_subjTrue_test_updated_v11_seed0_filt_multitask_fixed.json
 
 PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python src/eval/eval_vqa.py \
     --model_name_or_path "$output_dir"/hf \
@@ -47,8 +47,8 @@ PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python src/eval/eval_vqa.py \
     --max_length 512 \
     --proj_out_num 256 \
     --do_sample \
-    --output_dir $output_dir/eval_vqa \
+    --output_dir $output_dir/eval_vqa1 \
 
 PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python src/eval/eval_vqa_utils.py \
---output_dir $output_dir/eval_vqa \
+--output_dir $output_dir/eval_vqa1 \
 --gt_file $test_path \
